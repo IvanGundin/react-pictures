7633be91f75775853e056d3a4b6e4dc782838072
Иван Гундин <gia691992men@yandex.ru>
2022-04-06 20:58:30 +0300

    Добавили шапку и первую секцию

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-function App() {
-  const сontainer = document.getElementById('root');
-  fetch('https://jsonplaceholder.typicode.com/photos?_limit=30')
-    .then((response) => response.json())
-    .then((data) => {
-      const result = data.map(post => `<img src=${post.thumbnailUrl} class='pictureImg'>`);
-      сontainer.innerHTML += result.join(' ');
-    });
-};
+//Способ решения №1:
+//============================
 
-export default App
+// import './index.css';
+
+// function App() {
+//   const сontainer = document.getElementById('root');
+//   fetch('https://jsonplaceholder.typicode.com/photos?_limit=30')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const result = data.map(post => `<img src=${post.thumbnailUrl} class = 'pictureImg'>`);
+//       сontainer.innerHTML += result.join(' ');
+//     });
+// };
+
+// export default App
+
+
+
+//Способ решения №2 (РАБОТА НАД ОШИБКАМИ):
+//============================
+
+import './style.css'
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=30')
+      .then(response => response.json())
+      .then(setList);
+  }, []);
+
+  return (
+  //<div style={{ padding: '10px', display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>   - вариант стилизации
+    <div className={'cardList'}>
+      {list.map(todo =>
+     // <img style={{ padding: '5px' }} src={todo.thumbnailUrl} key={todo.id}></img>)}  - вариант стилизации
+        <img className={'card'} src={todo.thumbnailUrl} key={todo.id}></img>)}
+    </div>
+  );
+}
+export default App;
